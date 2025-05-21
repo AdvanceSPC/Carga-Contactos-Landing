3c8daa671f75a63dd89545464e241d193dcd2eaa
Layedra75 <layedra75@gmail.com>
2025-05-21 17:17:54 -0500

    landing

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ const app = express();
 // Configuración de vistas
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Configuración de variables de entorno
 dotenv.config({ path: './env/.env' });
@@ -22,10 +25,6 @@ app.use(fileUpload());
 // Cargar rutas
 const routes = require('./routes/routes');
 app.use('/', routes);
-
-app.use((req, res) => {
-  res.status(404).render("404");
-});
 
 app.listen(3000, () => {
   console.log('SERVER UP running in http://localhost:3000');
